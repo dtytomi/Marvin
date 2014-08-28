62559cc08eee14b1f92be273a00c6c418de99022
dtytomi <dtytomi@gmail.com>
2014-08-28 10:45:46 +0100

    add angular and fileupload dependencies to production config

--- config/env/production.js
+++ config/env/production.js
@@ -9,7 +9,9 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
 			],
 			js: [
-				'public/lib/angular/angularsss.min.js',
+				'public/lib/angular-file-upload/dist/angular-file-upload-html5-shim.min.js',			
+				'public/lib/angular/angular.js',
+				'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-cookies/angular-cookies.js', 
 				'public/lib/angular-animate/angular-animate.js', 
